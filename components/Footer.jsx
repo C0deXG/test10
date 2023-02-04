ceae7423e8c3a626482a248a629d72b8e4e46792
COdXG <85204159+C0deXG@users.noreply.github.com>
2023-02-04 11:21:13 -0600

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 const footer = (footerBanner) => {
   return (
     <div className='footer-container'>
-      <p>2022 mouse's All right reserverd @StrXth</p>
+      <p>2022 mouse's All right reserverd @StrXth00001</p>
       <p>
         <AiFillInstagram/>
         <AiOutlineTwitter/>
